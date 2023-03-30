8f51cee3eb2a6a17b12c53b0d8cecf7c180cca60
charleschile <charles.chi.le@outlook.com>
2023-03-30 16:17:16 +0800

    update

--- 704-binary-search.cpp
+++ 704-binary-search.cpp
@@ -1,4 +1,4 @@
-// 第一种解法
+// 第一种解法，左闭右闭
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -10,6 +10,25 @@ public:
             } else if (nums[mid] < target) {
                 left = mid + 1;
             } else return mid;
+        }
+        return -1;
+    }
+};
+
+// 第二种解法，左闭右开
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
         }
         return -1;
     }
