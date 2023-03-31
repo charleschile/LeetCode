3de744abc905ce38567c2fb2b37b4148af18a5b6
charleschile <charles.chi.le@outlook.com>
2023-04-01 00:06:47 +0800

    update

--- 367-valid-perfect-square.cpp
+++ 367-valid-perfect-square.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        int left = 1, right = num, mid, sqrt;
+        int left = 0, right = num;
         while (left <= right) {
-            mid = left + ((right - left) >> 1);
-            sqrt = num / mid;
-            if (sqrt > mid) {
-                left = mid + 1; 
-            } else if (sqrt < mid) {
+            int mid = (right - left) / 2 + left;
+            long square = (long) mid * mid;
+            if (square < num) {
+                left = mid + 1;
+            } else if (square > num) {
                 right = mid - 1;
-            } else if (sqrt == mid) {
-                if (sqrt * mid == num) return true;
-                else break;
+            } else {
+                return true;
             }
         }
-        double res = (double) num / right;
-        if (res * res == num) {
-            // cout << "right:" << right << "res:" << res << "sqrt: " << sqrt << endl; 
-            return true;
-        } else {
-            // cout << "right:" << right << "res:" << mid << "res: " << sqrt << endl; 
-            return false;
-        }
+        return false;
     }
 };
